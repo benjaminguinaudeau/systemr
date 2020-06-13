e89631d19df0e84875fd2c67e669b07eb3ff0a5b
benjaminguinaudeau <bguinaudeau@gmail.com>
2020-06-13 10:08:39 -0400

    ini

--- R/serve_cli.R
+++ R/serve_cli.R
@@ -1,7 +1,7 @@
 #' serve_cli
 #' @export
 serve_cli <- function(host = "localhost", port = 5000){
-  pr <- plumber::plumb("R/api.R")
+  pr <- plumber::plumb("https://github.com/benjaminguinaudeau/systemr/blob/master/R/api.R")
   pr$run(host = host, port = port, swagger = F)
 }
 
